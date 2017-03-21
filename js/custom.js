bdf1288e385ceac7ffee0fb8aca31a4cfe271a67
thalary <thomas.halary@devinci.fr>
2017-03-21 13:35:14 +0100

    js
    modif bon js

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,30 @@
+function fonctionRequeteApi(url,id){
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById(id).innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.open("GET", url , true);
 xmlhttp.send();
+}
+function homePageLoading(){
+var url ="http://bitcoin.mubiz.com/info";
+var elementID ="info";
+fonctionRequeteApi(url,elementID);
+
+var url ="http://bitcoin.mubiz.com/blockchaininfo";
+var elementID ="blockchaininfo";
+fonctionRequeteApi(url,elementID);
+
+var url ="http://bitcoin.mubiz.com/peerinfo";
+var elementID ="peerinfo";
+fonctionRequeteApi(url,elementID);
+
+var url ="http://bitcoin.mubiz.com/mininginfo";
+var elementID ="mininginfo";
+fonctionRequeteApi(url,elementID);
+}
+homePageLoading();
